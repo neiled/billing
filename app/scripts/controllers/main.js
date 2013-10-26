ef9dd7ee593e04e56cb51d7399031f9c51b27958
Neil Edwards <neil@Neils-MacBook-Pro.local>
2013-10-26 16:06:03 -0400

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('billingApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.people = [
+      'Neil',
+      'Laura'
+    ];
+  });
